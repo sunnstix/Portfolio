81933c363c78d51f2f0ba0c53b7e66bf8b90fb12
SunnyNayak <sanketn@umich.edu>
2021-06-23 14:37:32 -0400

    Removed keys

--- portfolio/config.py
+++ portfolio/config.py
@@ -6,10 +6,6 @@ from pathlib import Path
 # Root of this application, useful if it doesn't occupy an entire domain
 APPLICATION_ROOT = '/'
 
-# Secret key for encrypting cookies
-SECRET_KEY = b'\x8e\xe9\xb4(\x91\x07\x9d<\x82CP_\xe9\xa2/0\xf6Bmg\x96\x8a\t\x82'
-SESSION_COOKIE_NAME = 'login'
-
 # File Upload to var/uploads/
 UPLOAD_FOLDER = Path(os.path.join(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
